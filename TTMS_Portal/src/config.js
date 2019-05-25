f09ae0a64b4fab3c39327af0779faa0689915f94
wanshaobo <1647579959@qq.com>
2019-05-25 14:41:11 +0800

    前端搭建  导入设置 axio, vuerouter  vuetify matericss等

---
+++ TTMS_Portal/src/config.js
@@ -0,0 +1,13 @@
+const config ={
+  url: "http://localhost:8080",
+  theme:{
+    primary: "#2196F3",
+    secondary: "#455A64",
+    accent: "#9c27b0",
+    error: "#f44336",
+    warning: "#FFC107",
+    info: "#64B5F6",
+    success: "#4caf50"
+  },
+}
+export default config;
---
+++ TTMS_Portal/src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'    //导入vueRouter
+import './http'    //导入axios相关
+import config from "./config"
+import Vuetify from 'vuetify' //导入vuetify
+import 'vuetify/dist/vuetify.min.css'   //导入vuetifyCSS
+import './assets/material.css'
+
+Vue.use(Vuetify, { theme: config.theme})
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  router,
+  template: '<App/>'
+})
