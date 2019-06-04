00d0ada445a8faf4bae2f3e2b31d0e07cf572fe9
wanshaobo <1647579959@qq.com>
2019-06-04 17:35:44 +0800

    修改创建产品页面1.0.3

--- TTMS_Portal/src/pages/producemanage/product/createproduct.vue
+++ TTMS_Portal/src/pages/producemanage/product/createproduct.vue
@@ -17,13 +17,13 @@
               <el-row>
                 <el-col :span="6">
                   <el-form-item label="产品编号" class="left">
-                    <el-input v-model="form.user" placeholder="审批人"></el-input>
+                    <el-input v-model="form.productNumber" placeholder=""></el-input>
                   </el-form-item>
                 </el-col>
                 <el-col :span="6">
                   <el-form-item label="团号" class="pl-0" :rules="[
                      { required: true },]">
-                    <el-select v-model="selectedGroupId" placeholder="请选择产品团号">
+                    <el-select v-model="selectedGroupId"  @change="changeGroup(selectedGroupId)" placeholder="请选择产品团号">
                       <el-option v-for="item in groupOptions" :key="item.value" :label="item.label" :value="item.value"></el-option>
                     </el-select>
                   </el-form-item>
@@ -31,11 +31,11 @@
                 <el-col :span="8">
                   <el-form-item label="产品分类" class="pl-0" :rules="[
                     { required: true },]">
-                    <el-select v-model="selectedFirstCatId" placeholder="一级分类" class="wid">
-                      <el-option v-for="item in catOptions1" :key="item.value" :label="item.label" :value="item.value" @click="loadSecondCats(item.value)"></el-option>
+                    <el-select v-model="selectedFirstCatId" placeholder="一级分类" @change="loadSecondCats(selectedFirstCatId)" class="wid">
+                      <el-option v-for="item in catOptions1" :key="item.value" :label="item.label" :value="item.value" ></el-option>
                     </el-select>
-                    <el-select v-model="selectedSecondCatId" placeholder="二级分类" class="wid">
-                      <el-option v-for="item in catOptions2" :key="item.value"  :label="item.label" :value="item.value" @click="loadThridCats(item.value)"></el-option>
+                    <el-select v-model="selectedSecondCatId" placeholder="二级分类" @change="loadThridCats(selectedSecondCatId)" class="wid">
+                      <el-option v-for="item in catOptions2" :key="item.value"  :label="item.label" :value="item.value"></el-option>
                     </el-select>
                     <el-select v-model="selectedThirdCatId" placeholder="三级分类" class="wid">
                       <el-option v-for="item in catOptions3" :key="item.value" :label="item.label" :value="item.value"></el-option>
@@ -46,13 +46,13 @@
               <el-row>
                 <el-col :span="5">
                   <el-form-item label="产品负责人" class="right">
-                    <el-input v-model="form.user" placeholder="请输入产品负责人"></el-input>
+                    <el-input v-model="form.productChargerName" placeholder="请输入产品负责人" disabled></el-input>
                   </el-form-item>
                 </el-col>
                 <el-col :span="10">
                   <el-form-item label="产品名称" class="widwid left" :rules="[
                       { required: true },]">
-                    <el-input v-model="form.user" placeholder="请输入产品名称"></el-input>
+                    <el-input v-model="form.productName" placeholder="请输入产品名称"></el-input>
                   </el-form-item>
                 </el-col>
               </el-row>
@@ -66,53 +66,69 @@
                     <el-row>
                       <el-col :span="6">
                         <el-form-item label="服务开始时间" class="left" :rules="[ { required: true },]">
-                          <el-input v-model="form.user" placeholder=""></el-input>
-                        </el-form-item>
-                      </el-col>
-                      <el-col :span="6">
-                        <el-form-item label="预售数量" class="right" :rules="[ { required: true },]">
-                          <el-input v-model="form.user" placeholder=""></el-input>
+                            <el-date-picker
+                              v-model="form.serverStartTime"
+                              type="date"
+                              placeholder="">
+                          </el-date-picker>
                         </el-form-item>
                       </el-col>
                       <el-col :span="7">
                         <el-form-item label="服务结束时间" class="left" :rules="[ { required: true },]">
-                          <el-input v-model="form.user" placeholder=""></el-input>
+                          <el-date-picker
+                            v-model="form.serverEndTime"
+                            type="date"
+                            placeholder="">
+                          </el-date-picker>
+                        </el-form-item>
+                      </el-col>
+                      <el-col :span="6">
+                        <el-form-item label="预售数量" class="right" :rules="[ { required: true },]">
+                          <el-input v-model="form.preSellNumber" placeholder=""></el-input>
                         </el-form-item>
                       </el-col>
                     </el-row>
                     <el-row>
                       <el-col :span="6">
                         <el-form-item label="最低数量" class="right" :rules="[ { required: true },]">
-                          <el-input v-model="form.user" placeholder=""></el-input>
+                          <el-input v-model="form.lowestNumber" placeholder=""></el-input>
                         </el-form-item>
                       </el-col>
                       <el-col :span="6">
                         <el-form-item label="上架时间" class="left" :rules="[ { required: true },]">
-                          <el-input v-model="form.user" placeholder=""></el-input>
+                          <el-date-picker
+                            v-model="form.upTime"
+                            type="date"
+                            placeholder="">
+                          </el-date-picker>
                         </el-form-item>
                       </el-col>
                       <el-col :span="7">
                         <el-form-item label="下架时间" class="left" :rules="[ { required: true },]">
-                          <el-input v-model="form.user" placeholder=""></el-input>
+                          <el-date-picker
+                            v-model="form.downTime"
+                            type="date"
+                            placeholder="">
+                          </el-date-picker>
                         </el-form-item>
                       </el-col>
                     </el-row>
                     <el-row>
                       <el-col :span="6">
                         <el-form-item label="商品价格" class="right" :rules="[ { required: true },]">
-                          <el-input v-model="form.user" placeholder=""></el-input>
+                          <el-input v-model="form.price" placeholder=""></el-input>
                         </el-form-item>
                       </el-col>
                       <el-col :span="10">
                         <el-form-item label="特别提示" class="left widwid">
-                          <el-input v-model="form.user" placeholder="限6个汉字"></el-input>
+                          <el-input v-model="form.tip" placeholder="限6个汉字"></el-input>
                         </el-form-item>
                       </el-col>
                     </el-row>
                     <el-row>
                       <el-col :span="20">
                         <el-form-item label="产品简介" class="left widwid">
-                          <el-input type="textarea" v-model="form.user" size="large" placeholder=""></el-input>
+                          <el-input type="textarea" v-model="form.desc" size="large" placeholder=""></el-input>
                         </el-form-item>
                       </el-col>
                     </el-row>
@@ -121,7 +137,7 @@
                         <div></div>
                       </el-col>
                       <el-col :span="4">
-                        <el-button type="primary">创建产品</el-button>
+                        <el-button type="primary" @click="createProduct()">创建产品</el-button>
                       </el-col>
                     </el-row>
                   </el-form>
@@ -149,8 +165,21 @@
         selectedSecondCatId: '',
         selectedThirdCatId: '',
         selectedGroupId:"",
+        chargerName:"",
         form: {
-          user: '',
+          productNumber: '', //产品编号
+          productChargerId:"",//产品负责人Id
+          productChargerName:"",  //产品负责人
+          productName:"",  //产品名称
+          serverStartTime:"",   //服务开始时间
+          serverEndTime:"",     //服务结束时间
+          preSellNumber:"",   //预售数量
+          lowestNumber:"",  //最低数量
+          upTime:"",      //上架时间
+          downTime:"",    //下架时间
+          price:"",       //价格
+          tip:"",       //提示
+          desc:"",      //描述
           region: ''
         }
 
@@ -193,7 +222,6 @@
       },
       //加载二级分类
       loadSecondCats(pid){
-        console("loadSecondCats");
         this.loadNCatById(pid,2);
       },
       //加载三级分类
@@ -210,6 +238,40 @@
               tempGroups.push(tempGroup);
             })
            this.groupOptions = tempGroups;
+        }).catch(error=>{
+
+        })
+      },
+      //改变要创建团的产品
+      changeGroup(groupId){
+        this.$http.get("/producemanage/product/createproduct/AmIcharger/"+groupId).then(resp=>{
+            this.form.id = resp.data.id;
+            this.form.productChargerName = resp.data.username;
+        }).catch(error=>{
+            this.$message.error(error.message);
+        })
+      },
+      //创建产品
+      createProduct(){
+        this.$http.post("/producemanage/product/createproduct/createProduct",this.$qs.stringify({
+          groupId:this.selectedGroupId,
+          productCatId1:this.selectedFirstCatId,
+          productCatId2:this.selectedSecondCatId,
+          productCatId3:this.selectedThirdCatId,
+          productName:this.form.productName,
+          serverStartTime:new Date(this.form.serverStartTime).format("yyyy-MM-dd hh:mm:ss"),
+          serverEndTime: new Date(this.form.serverEndTime).format("yyyy-MM-dd hh:mm:ss"),
+          preSellNumber:this.form.preSellNumber,
+          lowestNumber:this.form.lowestNumber,
+          selledNumber:"0",
+          onsellTime:new Date(this.form.upTime).format("yyyy-MM-dd hh:mm:ss"),
+          productPrice:this.form.price,
+          upsellTime:new Date(this.form.downTime).format("yyyy-MM-dd hh:mm:ss"),
+          hotTip:this.form.tip,
+          productIntroduction:this.form.desc,
+
+        })).then(resp=>{
+            console.log(resp.data);
         }).catch(error=>{
 
         })
