ce8802972366544aad0959351f4c4c63baa2f3ed
wanshaobo <1647579959@qq.com>
2019-06-05 17:10:38 +0800

    角色管理页面1.0.2

--- TTMS_Portal/src/pages/sysmanage/userauth/rolemanage.vue
+++ TTMS_Portal/src/pages/sysmanage/userauth/rolemanage.vue
@@ -1,115 +1,114 @@
 <template>
   <el-container>
-
     <el-container>
-
-      <el-main><div class="top"><p class="title" style="color:#B3C0D1">角色信息管理</p>
-        <div class="path" ><el-breadcrumb separator-class="el-icon-arrow-right">
-          <el-breadcrumb-item :to="{ path: '/' }">信息管理</el-breadcrumb-item>
-          <el-breadcrumb-item>角色</el-breadcrumb-item>
-          <el-breadcrumb-item>角色信息管理</el-breadcrumb-item>
-
-        </el-breadcrumb></div>
-        <el-row :gutter="20">
-
-          <el-col :span="5"><div class="grid-content "><el-input v-model="name" placeholder="角色名称"></el-input>
-          </div></el-col>
-          <el-col :span="2"><el-button type="primary" @click="loadData()">查询</el-button><div class="grid-content "></div></el-col>
-          <el-col :span="2"><div class="grid-content ">
-            <el-button type="primary" @click="dialogFormVisible = true">新增</el-button>
-
-            <el-dialog title="添加角色" :visible.sync="dialogFormVisible">
-              <el-form ref="form" :model="form" label-width="80px">
-                <el-form-item label="活动名称">
-                  <el-input v-model="form.name"></el-input>
-                </el-form-item>
-                <el-form-item label="备注">
-                  <el-input v-model="form.comment"></el-input>
-                </el-form-item>
-                <el-form-item label="父分类:" >
-                  <div class="left"> <el-select v-model="value" placeholder="请选择">
-                    <el-option
-                      v-for="item in options"
-                      :key="item.value"
-                      :label="item.label"
-                      :value="item.value">
-
-                    </el-option>
-                  </el-select></div>
-                </el-form-item>
-                <el-form-item label="子分类:" >
-                  <div class="left"><el-select v-model="value" placeholder="请选择">
-                    <el-option
-                      v-for="item in options"
-                      :key="item.value"
-                      :label="item.label"
-                      :value="item.value">
-                    </el-option>
-                  </el-select></div>
-                </el-form-item>
-
-                <el-form-item label="授权">
-                  <el-tree
-                    :data="data"
-                    show-checkbox
-                    node-key="id"
-                    :default-expanded-keys="[2, 3]"
-                    :default-checked-keys="[5]">
-                  </el-tree>
-                </el-form-item>
-
-              </el-form>
-              <div slot="footer" class="dialog-footer">
-                <el-button @click="dialogFormVisible = false">取 消</el-button>
-                <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+      <el-main>
+        <div class="top">
+          <p class="title" style="color:#B3C0D1">角色信息管理</p>
+          <div class="path">
+            <el-breadcrumb separator-class="el-icon-arrow-right">
+              <el-breadcrumb-item :to="{ path: '/' }">
+                信息管理
+              </el-breadcrumb-item>
+              <el-breadcrumb-item>
+                角色
+              </el-breadcrumb-item>
+              <el-breadcrumb-item>
+                角色信息管理
+              </el-breadcrumb-item>
+            </el-breadcrumb>
+          </div>
+          <el-row :gutter="20">
+            <el-col :span="5">
+              <div class="grid-content ">
+                <el-input v-model="name" placeholder="角色名称"></el-input>
               </div>
-            </el-dialog>
-          </div></el-col>
-          <el-col :span="2"><el-button type="primary">修改</el-button><div class="grid-content "></div></el-col>
-          <el-col :span="2"><el-button type="primary" @click="deleteRow(radio)">删除</el-button><div class="grid-content "></div></el-col>
-        </el-row></div>
+            </el-col>
+            <el-col :span="2">
+              <el-button type="primary" @click="loadData()">
+                查询
+              </el-button>
+              <div class="grid-content "></div>
+            </el-col>
+            <el-col :span="2">
+              <div class="grid-content ">
+                <el-button type="primary" @click="dialogFormVisible = true">
+                  新增
+                </el-button>
+                <el-dialog title="添加角色" :visible.sync="dialogFormVisible">
+                  <el-form ref="form" :model="form" label-width="80px">
+                    <el-form-item label="角色名称">
+                      <el-input v-model="form.name"></el-input>
+                    </el-form-item>
+                    <el-form-item label="角色备注">
+                      <el-input v-model="form.comment"></el-input>
+                    </el-form-item>
+                    <el-form-item label="父部门:">
+                      <div class="left">
+                        <el-select v-model="selectedParentDepartId" @change="loadChildDepartment(selectedParentDepartId)" placeholder="请选择">
+                          <el-option v-for="item in parentDepartOptions" :key="item.value" :label="item.label" :value="item.value">
+                          </el-option>
+                        </el-select>
+                      </div>
+                    </el-form-item>
+                    <el-form-item label="子部门:">
+                      <div class="left">
+                        <el-select v-model="selectedChildDepartId"  placeholder="请选择">
+                          <el-option v-for="item in childDepartOptions" :key="item.value" :label="item.label" :value="item.value">
+                          </el-option>
+                        </el-select>
+                      </div>
+                    </el-form-item>
+                    <el-form-item label="权限菜单">
+                      <el-tree :data="menuDate" show-checkbox="" ref="tree" node-key="id" :default-expanded-keys="[]" :default-checked-keys="[]">
+                      </el-tree>
+                    </el-form-item>
+                  </el-form>
+                  <div slot="footer" class="dialog-footer">
+                    <el-button @click="dialogFormVisible = false">
+                      取 消
+                    </el-button>
+                    <el-button type="primary" @click="createRole()">
+                      确 定
+                    </el-button>
+                  </div>
+                </el-dialog>
+              </div>
+            </el-col>
+            <el-col :span="2">
+              <el-button type="primary">
+                修改
+              </el-button>
+              <div class="grid-content "></div>
+            </el-col>
+            <el-col :span="2">
+              <el-button type="primary" @click="deleteRow(radio)">
+                删除
+              </el-button>
+              <div class="grid-content "></div>
+            </el-col>
+          </el-row>
+        </div>
         <div class="body">
-          <el-table
-            :data="tableData"
-            @row-click = "showRow"
-            style="width: 100%">
+          <el-table :data="tableData" @row-click="showRow" style="width: 100%">
             <el-table-column label="选择" width="180" align="center">
               <template slot-scope="scope">
-                <el-radio class="radio"  v-model="radio"  :label="scope.$index">&nbsp;</el-radio>
+                <el-radio class="radio" v-model="radio" :label="scope.$index">
+                  &nbsp;
+                </el-radio>
               </template>
             </el-table-column>
-
-            <el-table-column
-              prop="roleID"
-              label="角色ID"
-              width="270">
+            <el-table-column prop="roleID" label="角色ID" width="270">
             </el-table-column>
-            <el-table-column
-              prop="roleName"
-              label="角色名称"
-              width="270">
+            <el-table-column prop="roleName" label="角色名称" width="270">
             </el-table-column>
-            <el-table-column
-              prop="comment"
-              label="备注"
-              width="300">
+            <el-table-column prop="comment" label="备注" width="300">
             </el-table-column>
           </el-table>
-
-
           <div class="page">
-            <el-pagination
-              @size-change="handleSizeChange"
-              @current-change="handleCurrentChange"
-              :current-page="currentPage"
-              :page-sizes="[5, 7, 9, 11]"
-              :page-size="5"
-              layout="total, sizes, prev, pager, next, jumper"
-              :total="totalItem">
+            <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="currentPage" :page-sizes="[5, 10, 15, 20]" :page-size="5" layout="total, sizes, prev, pager, next, jumper" :total="totalItem">
             </el-pagination>
           </div>
         </div>
-
       </el-main>
     </el-container>
   </el-container>
@@ -122,202 +121,6 @@
     data() {
       return {
         id:'',
-        options: [{
-          value: '选项1',
-          label: '黄金糕'
-        }, {
-          value: '选项2',
-          label: '双皮奶'
-        }, {
-          value: '选项3',
-          label: '蚵仔煎'
-        }, {
-          value: '选项4',
-          label: '龙须面'
-        }, {
-          value: '选项5',
-          label: '北京烤鸭'
-        }],
-        value: '',
-        data: [{
-          id: 1,
-          label: '信息面板',
-          children: [{
-            id: 2,
-            label: '信息面板',
-            children: [{
-              id: 3,
-              label: '发布通知'
-            }, {
-              id: 4,
-              label: '通知管理',
-              disabled: true
-            }]
-          },]
-        },
-          {
-            id: 5,
-            label: '消息中心',
-            children: [{
-              id: 6,
-              label: '通知管理',
-              children: [{
-                id: 7,
-                label: '日志管理'
-              }, ]
-            },]
-          },
-          {
-            id: 8,
-            label: '产品管理',
-            children: [{
-              id: 9,
-              label: '项目信息',
-              children: [{
-                id: 10,
-                label: '新增'
-              }, {
-                id: 11,
-                label: '启用',
-                disabled: true
-              },
-                {
-                  id: 12,
-                  label: '禁用',
-                  disabled: true
-                },
-                {
-                  id: 13,
-                  label: '修改',
-                  disabled: true
-                }]
-            },
-              {
-                id: 14,
-                label: '团号',
-                children: [{
-                  id: 10,
-                  label: '团号管理'
-                }]
-              },
-              {
-                id: 15,
-                label: '产品',
-                children: [{
-                  id: 10,
-                  label: '创建产品'
-                },{
-                  id: 10,
-                  label: '产品列表'
-                },{
-                  id: 10,
-                  label: '产品分类'
-                }]
-              },
-              {
-                id: 14,
-                label: '政策',
-                children: [{
-                  id: 10,
-                  label: '价格政策'
-                }]
-              },
-            ]
-          },
-          {
-            id: 1,
-            label: '销售订单',
-            children: [{
-              id: 3,
-              label: '订单',
-              children: [{
-                id: 4,
-                label: '订单管理'
-              }, ]
-            },]
-          },
-          {
-            id: 1,
-            label: '资源管理',
-            children: [{
-              id: 3,
-              label: '信息面板',
-              children: [{
-                id: 4,
-                label: '发布通知'
-              }, {
-                id: 5,
-                label: '通知管理',
-                disabled: true
-              }]
-            },]
-          },
-          {
-            id: 1,
-            label: '信息面板',
-            children: [{
-              id: 3,
-              label: '信息面板',
-              children: [{
-                id: 4,
-                label: '发布通知'
-              }, {
-                id: 5,
-                label: '通知管理',
-                disabled: true
-              }]
-            },]
-          },
-          {
-            id: 1,
-            label: '信息面板',
-            children: [{
-              id: 3,
-              label: '信息面板',
-              children: [{
-                id: 4,
-                label: '发布通知'
-              }, {
-                id: 5,
-                label: '通知管理',
-                disabled: true
-              }]
-            },]
-          },
-          {
-            id: 1,
-            label: '信息面板',
-            children: [{
-              id: 3,
-              label: '信息面板',
-              children: [{
-                id: 4,
-                label: '发布通知'
-              }, {
-                id: 5,
-                label: '通知管理',
-                disabled: true
-              }]
-            },]
-          },
-          {
-            id: 1,
-            label: '信息面板',
-            children: [{
-              id: 3,
-              label: '信息面板',
-              children: [{
-                id: 4,
-                label: '发布通知'
-              }, {
-                id: 5,
-                label: '通知管理',
-                disabled: true
-              }]
-            },]
-          },
-
-        ],
         defaultProps: {
           children: 'children',
           label: 'label'
@@ -329,25 +132,18 @@
         checkedRoles: ['系统管理员', '产品经理'],
         roles: roleOptions,
         dialogFormVisible: false,
-        form: {
-          id:'',
-          password:'',
-          email:'',
-          phoneNum:'',
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: ''
-        },
         formLabelWidth: '120px',
-        tableData: [],
+        tableData: [],  //表格数据
         multipleSelection: [],
         radio: '',
-        selected:{},
-        input1: '',
+        childDepartOptions:[],  //子选项
+        parentDepartOptions:[],  //父选项
+        selectedParentDepartId:"", //父Id
+        selectedChildDepartId:"",  //子Id
+        menuDate: [],   //菜单项
+        extractIdMenus:[],  //提取权限菜单id所用的临时对象  格式[{ids:[1,2,3],isExtract:false}]
+        isEdit:false,         //是否是编辑状态
+        dialogName:"新增角色",
         currentPage: 1,  //当前页
         rows:5,    //每页大小
         totalItem : 20,   //总条数
@@ -355,7 +151,12 @@
       };
     },
     created(){
+      //加载表单中的数据
       this.loadData();
+      //加载菜单树
+      this.loadMenuTree();
+      //加载一级部门
+      this.loadParentByPidInOptions(0,1);
     },
     methods: {
       showRow(row){
@@ -380,6 +181,42 @@
         this.currentPage = val;
         this.loadData();
       },
+      //获取有效的菜单Id
+      getCheckedKeys() {
+        var selectedMenusKeys = this.$refs.tree.getCheckedKeys();
+        //选出能够确定角色特权的菜单key
+        var availableIds = [];
+        this.menuDate.forEach(module=>{
+          var moduleId = module.id;
+          //模块
+          if(selectedMenusKeys.includes(moduleId)){
+            //如果一级被选中
+            availableIds.push(moduleId);
+            return ;
+          }else{
+            //如果一级没有被选中
+            module.children.forEach(menu=>{
+              var menuId = menu.id;
+              if(selectedMenusKeys.includes(menuId)){
+                //二级被选中了
+                availableIds.push(menuId);
+                return;
+              }else{
+                //二级没有被选中
+                menu.children.forEach(item=>{
+                  var itemId = item.id;
+                  if(selectedMenusKeys.includes(itemId)){
+                    //三级选中了
+                    availableIds.push(itemId);
+                  }
+                })
+              }
+            })
+          }
+        });
+        //赋值给extractIdMenus
+        this.extractIdMenus = availableIds;
+      },
       deleteRow(index){
         this.tableData.splice(index,1)
         //alert(index)
@@ -394,7 +231,7 @@
                 return true
               }
             })
-*/
+        */
             return true
           }
         })
@@ -409,7 +246,6 @@
           }
         }).then(resp => {
           //成功
-          console.log(resp);
           this.totalItem = resp.data.total;
           var tables = [];
           resp.data.items.forEach(roleItem => {
@@ -424,6 +260,85 @@
           alert(error.message);
         });
       },
+      //加载权限菜单树
+      loadMenuTree(){
+        this.$http.get("/sysmanage/userauth/rolemanage/tree").then(resp=>{
+           var modules = [];
+           var tempExtractIdMenus = [];
+          resp.data.forEach(module=>{
+            //模块
+            var mod = {};
+            mod.id = module.id;
+            mod.label = module.name;
+            mod.isExtract = false;
+            mod.children=[];
+            module.childMenus.forEach(menu=>{
+              var men = {};
+              men.id = menu.id;
+              men.label = menu.name;
+              men.isExtract = false;
+              men.children = [];
+              menu.childMenus.forEach(item=>{
+                var ite = {};
+                ite.id = item.id;
+                ite.label = item.name;
+                ite.isExtract = false;
+                men.children.push(ite);
+              })
+              mod.children.push(men);
+            })
+            modules.push(mod);
+          });
+          this.menuDate= modules;
+        }).catch(error=>{
+
+        })
+      },
+      //加载部门信息到opt中
+      loadParentByPidInOptions(pid , level){
+        this.$http.get("/sysmanage/userauth/rolemanage/getDepartmentBypid",{
+          params:{
+            pid:pid,
+          }
+        }).then(resp=>{
+          var tempDeparts =[];
+          resp.data.forEach(item=>{
+            var tempDepart ={};
+            tempDepart.value = item.id;
+            tempDepart.label = item.departmentname;
+            tempDeparts.push(tempDepart);
+          })
+          if(level == 1){
+            this.parentDepartOptions = tempDeparts;
+          }else{
+            this.childDepartOptions = tempDeparts;
+          }
+        }).catch(error=>{
+          this.$message.error(error.message);
+        });
+      },
+      //根据父id加载子分类
+      loadChildDepartment(pid){
+        this.selectedChildDepartId = "";
+        this.loadParentByPidInOptions(pid,2);
+      },
+      //创建角色
+      createRole(){
+        //获取选中的有效菜单id
+        this.getCheckedKeys();
+        this.$http.post("/sysmanage/userauth/rolemanage",this.$qs.stringify(
+          {
+            name:this.form.name,
+            note:this.form.comment,
+            menuIds:this.extractIdMenus.join(","),
+            departmentId:this.selectedChildDepartId
+          }
+        )).then(resp=>{
+            this.$message.error("添加成功");
+        }).catch(error=>{
+
+        })
+      }
     },
   };
 </script>
