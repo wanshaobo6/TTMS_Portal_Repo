6f661bc1854e83ccd658fef8c7cc8aa7f24bf4d9
wanshaobo <1647579959@qq.com>
2019-06-06 15:10:52 +0800

    角色管理页面1.0.6

--- TTMS_Portal/src/pages/sysmanage/userauth/usermanage.vue
+++ TTMS_Portal/src/pages/sysmanage/userauth/usermanage.vue
@@ -1,121 +1,126 @@
 <template>
   <el-container>
-
     <el-container>
-
-      <el-main><div class="top"><p class="title" style="color:#B3C0D1">用户信息管理</p>
-        <div class="path" ><el-breadcrumb separator-class="el-icon-arrow-right">
-          <el-breadcrumb-item :to="{ path: '/' }">信息管理</el-breadcrumb-item>
-          <el-breadcrumb-item>用户</el-breadcrumb-item>
-          <el-breadcrumb-item>用户信息管理</el-breadcrumb-item>
-
-        </el-breadcrumb></div>
-        <el-row :gutter="20">
-
-          <el-col :span="4"><div class="grid-content "><el-input v-model="name" placeholder="用户名"></el-input>
-          </div></el-col>
-          <el-col :span="2"><el-button type="primary" @click="loadData()">查询</el-button><div class="grid-content "></div></el-col>
-          <el-col :span="2"><div class="grid-content ">
-            <el-button type="primary" @click="showDialog(false)">新增</el-button>
-
-            <el-dialog title="新增用户" :visible.sync="dialogFormVisible">
-              <el-form :model="form" >
-                <el-form-item label="照片" >
-                  <el-upload
-                    class="avatar-uploader"
-                    action="https://jsonplaceholder.typicode.com/posts/"
-                    :show-file-list="false"
-                    :on-success="handleAvatarSuccess"
-                    :before-upload="beforeAvatarUpload">
-                    <img v-if="imageUrl" :src="imageUrl" class="avatar">
-                    <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-                  </el-upload>
-                </el-form-item>
-                <el-form-item label="用户名:" :rules="[
-							  { required: true },]">
-                  <el-input v-model="form.usename" placeholder="登录账号"></el-input>
-                </el-form-item>
-                <el-form-item label="密码:" :rules="[
-							  { required: true },]">
-                  <el-input v-model="form.password" placeholder="密码"></el-input>
-                </el-form-item>
-                <el-form-item label="邮箱:">
-                  <el-input v-model="form.email" placeholder="邮箱"></el-input>
-                </el-form-item>
-                <el-form-item label="手机号:" :rules="[
-											  { required: true },]">
-                  <el-input v-model="form.phoneNum" placeholder="手机号"></el-input>
-                </el-form-item>
-                <el-form-item label="父部门:" >
-                  <div class="left"> <el-select v-model="selectedParentDepartId" @change="loadChildDepartByPid(selectedParentDepartId)" placeholder="请选择">
-                    <el-option
-                      v-for="item in parentDepartOptions"
-                      :key="item.value"
-                      :label="item.label"
-                      :value="item.value">
-                    </el-option>
-                  </el-select></div>
-                </el-form-item>
-                <el-form-item label="子部门:" >
-                  <div class="left"><el-select v-model="selectedChildDepartId" @change="loadRolesBySubDepartId(selectedChildDepartId)" placeholder="请选择">
-                    <el-option
-                      v-for="item in childDepartOptions"
-                      :key="item.value"
-                      :label="item.label"
-                      :value="item.value">
-                    </el-option>
-                  </el-select></div>
-                </el-form-item>
-                <el-form-item label="角色:">
-                  <div class="roles">
-                    <el-radio v-model="selectedRole" v-for=" role in roles" :key="role.value"  :label="role.value" >{{role.label}}</el-radio>
-                  </div>
-                </el-form-item>
-
-              </el-form>
-              <div slot="footer" class="dialog-footer">
-                <el-button @click="dialogFormVisible = false">取 消</el-button>
-                <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+      <el-main>
+        <div class="top">
+          <p class="title" style="color:#B3C0D1">用户信息管理</p>
+          <div class="path">
+            <el-breadcrumb separator-class="el-icon-arrow-right">
+              <el-breadcrumb-item :to="{ path: '/' }">
+                信息管理
+              </el-breadcrumb-item>
+              <el-breadcrumb-item>
+                用户
+              </el-breadcrumb-item>
+              <el-breadcrumb-item>
+                用户信息管理
+              </el-breadcrumb-item>
+            </el-breadcrumb>
+          </div>
+          <el-row :gutter="20">
+            <el-col :span="4">
+              <div class="grid-content ">
+                <el-input v-model="name" placeholder="用户名"></el-input>
               </div>
-            </el-dialog>
-          </div></el-col>
-          <el-col :span="2"><el-button type="primary">修改</el-button><div class="grid-content "></div></el-col>
-        </el-row></div>
+            </el-col>
+            <el-col :span="2">
+              <el-button type="primary" @click="loadData()">
+                查询
+              </el-button>
+              <div class="grid-content "></div>
+            </el-col>
+            <el-col :span="2">
+              <div class="grid-content ">
+                <el-button type="primary" @click="showDialog(false)">
+                  新增
+                </el-button>
+                <el-dialog title="新增用户" :visible.sync="dialogFormVisible">
+                  <el-form :model="form">
+                    <el-form-item label="照片">
+                      <el-upload class="avatar-uploader" action="http://localhost:8080/upload" :show-file-list="false" :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
+                        <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+                        <i v-else="" class="el-icon-plus avatar-uploader-icon"></i>
+                      </el-upload>
+                    </el-form-item>
+                    <el-form-item label="用户名:" :rules="[
+							            { required: true },]">
+                      <el-input v-model="form.usename" placeholder="登录账号"></el-input>
+                    </el-form-item>
+                    <el-form-item label="密码:" :rules="[
+							           { required: true },]">
+                      <el-input v-model="form.password" placeholder="密码"></el-input>
+                    </el-form-item>
+                    <el-form-item label="邮箱:">
+                      <el-input v-model="form.email" placeholder="邮箱"></el-input>
+                    </el-form-item>
+                    <el-form-item label="手机号:" :rules="[
+											  { required: true },]">
+                      <el-input v-model="form.phoneNum" placeholder="手机号"></el-input>
+                    </el-form-item>
+                    <el-form-item label="父部门:">
+                      <div class="left">
+                        <el-select v-model="selectedParentDepartId" @change="loadChildDepartByPid(selectedParentDepartId)" placeholder="请选择">
+                          <el-option v-for="item in parentDepartOptions" :key="item.value" :label="item.label" :value="item.value">
+                          </el-option>
+                        </el-select>
+                      </div>
+                    </el-form-item>
+                    <el-form-item label="子部门:">
+                      <div class="left">
+                        <el-select v-model="selectedChildDepartId" @change="loadRolesBySubDepartId(selectedChildDepartId)" placeholder="请选择">
+                          <el-option v-for="item in childDepartOptions" :key="item.value" :label="item.label" :value="item.value">
+                          </el-option>
+                        </el-select>
+                      </div>
+                    </el-form-item>
+                    <el-form-item label="角色:">
+                      <div class="roles">
+                        <el-radio v-model="selectedRole" v-for=" role in roles" :key="role.value" :label="role.value">
+                          {{role.label}}
+                        </el-radio>
+                      </div>
+                    </el-form-item>
+                  </el-form>
+                  <div slot="footer" class="dialog-footer">
+                    <el-button @click="dialogFormVisible = false">
+                      取 消
+                    </el-button>
+                    <el-button type="primary" @click="submitForm()">
+                      确 定
+                    </el-button>
+                  </div>
+                </el-dialog>
+              </div>
+            </el-col>
+            <el-col :span="2">
+              <el-button type="primary">
+                修改
+              </el-button>
+              <div class="grid-content "></div>
+            </el-col>
+          </el-row>
+        </div>
         <div class="body">
-          <el-table
-            :data="tableData"
-            @row-click = "showRow"
-            border
-            style="width: 100%">
+          <el-table :data="tableData" @row-click="showRow" border="" style="width: 100%">
             <el-table-column label="选择" width="80" align="center">
               <template slot-scope="scope">
-                <el-radio class="radio"  v-model="radio"  :label="scope.$index">&nbsp;</el-radio>
+                <el-radio class="radio" v-model="radio" :label="scope.$index">
+                  &nbsp;
+                </el-radio>
               </template>
             </el-table-column>
-            <el-table-column
-              prop="image"
-              label="照片"
-              width="150">
+            <el-table-column prop="image" label="照片" width="150">
+                <template slot-scope="scope">
+                  <img :src="scope.row.image" width="100px" height="50px" style="margin-left:15px;line-height: 50px "></img>
+                </template>
             </el-table-column>
-            <el-table-column
-              prop="username"
-              label="用户名"
-              width="180">
+            <el-table-column prop="username" label="用户名" width="180">
             </el-table-column>
-            <el-table-column
-              prop="email"
-              label="邮箱"
-              width="230">
+            <el-table-column prop="email" label="邮箱" width="230">
             </el-table-column>
-            <el-table-column
-              prop="phoneNum"
-              label="手机号"
-              width="180">
+            <el-table-column prop="phoneNum" label="手机号" width="180">
             </el-table-column>
-            <el-table-column
-              prop="status"
-              label="状态"
-              width="165">
+            <el-table-column prop="status" label="状态" width="165">
               <template slot-scope="scope">
                 <span v-show="scope.row.status==1" style="color: green">启用</span>
                 <span v-show="scope.row.status!=1" style="color: red">禁用</span>
@@ -123,24 +128,20 @@
             </el-table-column>
             <el-table-column label="操作" width="120">
               <template slot-scope="scope">
-                <el-button size="mini" type="danger" v-show="scope.row.status == 1"  @click="changeStatus(scope.row)">禁用</el-button>
-                <el-button size="mini" type="success" v-show="scope.row.status != 1" @click="changeStatus(scope.row)">启用</el-button>
+                <el-button size="mini" type="danger" v-show="scope.row.status == 1" @click="changeStatus(scope.row)">
+                  禁用
+                </el-button>
+                <el-button size="mini" type="success" v-show="scope.row.status != 1" @click="changeStatus(scope.row)">
+                  启用
+                </el-button>
               </template>
             </el-table-column>
           </el-table>
           <div class="page">
-            <el-pagination
-              @size-change="handleSizeChange"
-              @current-change="handleCurrentChange"
-              :current-page="currentPage"
-              :page-sizes="[5, 10, 15, 20]"
-              :page-size="5"
-              layout="total, sizes, prev, pager, next, jumper"
-              :total="totalItem">
+            <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="currentPage" :page-sizes="[5, 10, 15, 20]" :page-size="5" layout="total, sizes, prev, pager, next, jumper" :total="totalItem">
             </el-pagination>
           </div>
         </div>
-
       </el-main>
     </el-container>
   </el-container>
@@ -232,7 +233,7 @@
             table.email = userItem.email;
             table.phoneNum = userItem.mobile;
             table.status= userItem.valid;
-
+            table.image = userItem.image
 
             tables.push(table);
           });
@@ -250,7 +251,7 @@
         })
       },
       handleAvatarSuccess(res, file) {
-        this.imageUrl = URL.createObjectURL(file.raw);
+        this.imageUrl = res;
       },
       beforeAvatarUpload(file) {
         const isJPG = file.type === 'image/jpeg';
@@ -300,6 +301,7 @@
         //加载二级
         this.loadDepartment(pid,2);
       },
+      //通过部门id加载角色
       loadRolesBySubDepartId(pid){
         this.$http.get("/sysmanage/userauth/usermanage/getRolesByDepartmentId",{
           params:{
@@ -316,6 +318,21 @@
         }).catch(error=>{
           this.$message.error(error.message);
         });
+      },
+      //提交按钮
+      submitForm(){
+        //todo 判断表单数据
+        if(!this.isEdit){
+          //新增
+          this.$http.post("/sysmanage/userauth/usermanage",this.$qs.stringify({
+            username:this.form.usename,
+            image:this.imageUrl,
+            password:this.form.password,
+            mail:this.form.email,
+            phonenumber:this.form.phoneNum,
+            roleId:this.selectedRole
+          }))
+        }
       }
     }
 
