0b14fe32794f87e565e84a845866b9f6e2a43a66
wubin <1947832562@qq.com>
2019-06-14 15:34:05 +0800

    改变项目的状态完善

--- TTMS_Portal/src/pages/DistributorEntry/DistributorInterface.vue
+++ TTMS_Portal/src/pages/DistributorEntry/DistributorInterface.vue
@@ -49,7 +49,7 @@
       </div>
       <div class="firstRow" style="margin-top: 60px">
         <el-row :gutter="20">
-          <el-col :span="3"><div class="grid-content "><el-select v-model="status" placeholder="状态">
+          <el-col :span="3"><div class="grid-content "><el-select v-model="status"  placeholder="状态">
             <el-option
               v-for="item in options"
               :key="item.value"
@@ -376,7 +376,7 @@
           resp.data.items.forEach(listItem => {
             var table = {};
             table.id = listItem.id;
-            table.status = listItem.productstatus;
+            table.status = listItem.productstatus==0 ?"待售":"待售" && listItem.productstatus==1 ?"上架":"下架" && listItem.productstatus==2 ?"下架":"下架";
             table.classify = listItem.productcatnames;
             table.project = listItem.projectname;
             table.Tname = listItem.groupname;
